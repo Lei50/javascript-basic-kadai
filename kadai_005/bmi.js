bc7d3bb340834bc690e5948c4eb5fc112ed65404
Lei50 <shishirarure826@gmail.com>
2025-01-12 08:24:17 +0900

    課題の初回提出

---
+++ kadai_005/bmi.js
@@ -0,0 +1,13 @@
+// 変数の宣言
+let bodyWeight;
+
+// 値の代入
+bodyWeight = 65;
+
+// 変数の宣言・値の代入
+let Height = 1.7;
+
+// コンソールへの出力
+console.log(bodyWeight+'kg');
+console.log(Height+'m');
+console.log(bodyWeight/(Height*Height));
